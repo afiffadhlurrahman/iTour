19a68688863491b3c20684b88acf270fa686489a
afiffadhlurrahman <afif.fadhlurrahman2@gmail.com>
2024-07-11 19:36:56 +0700

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Muhammad Afif Fadhlurrahman on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
